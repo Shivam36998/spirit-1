6f9cf3ea9f7b032b294b46a5c8446b417175bb08
iitian360 <sp48840@gmail.com>
2022-11-30 14:27:43 +0530

    all related page have been added

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,6 +1,18 @@
+class home_event{
+    static My_home= (req,res)=>{
+        res.render('home',{'title':'Home'});
+    }
+    static My_gallery=(req,res)=>{
+        res.render('gallery',{'title':'Gallery'})
+    }
 
-const My_home= (req,res)=>{
-    res.render('home',{'title':'Home'});
+    static My_login=(req,res)=>{
+        res.render('login',{'title':'login here'})
+    }
+    static My_registration=(req,res)=>{
+        res.render('registration',{'title':'welcome!'})
+    }
 }
 
-export default My_home;
+
+export default home_event;
--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import My_home from '../controller/home_controller.js';
+import home_event from '../controller/home_controller.js';
 const router=express.Router();
 
-router.get('/',My_home);
-router.get('/home',My_home);
+router.get('/',home_event.My_home);
+router.get('/home',home_event.My_home);
+router.get('/gallery',home_event.My_gallery);
+router.get('/login',home_event.My_login);
+router.get('/registration',home_event.My_registration);
 
 
 export default router;
